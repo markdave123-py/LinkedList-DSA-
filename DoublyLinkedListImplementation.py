7ee5200e03d4d2e598d03197256eae0709f84ab9
markdave123-py <nwaewkudavid@gmail.com>
2023-04-12 19:02:57 +0100

    made changes

--- DoublyLinkedListImplementation.py
+++ DoublyLinkedListImplementation.py
@@ -1,47 +1,3 @@
-# class SinglyLinkedList(object):
-#     def __init__(self, value):
-#         self.value = value
-#         self.next = None
-
-
-# L1 = SinglyLinkedList(1)
-# L2 = SinglyLinkedList(2)
-# L3 = SinglyLinkedList(3)
-
-# L1.next = L2
-# L2.next = L3
-
-
-
-# class DoublyLinkedList(object):
-#     def __init__(self, value):
-#         self.value = value
-#         self.next = None
-#         self.prevNode = None
-
-# L1 = DoublyLinkedList(1)
-# L2 = DoublyLinkedList(2)
-# L3 = DoublyLinkedList(3)
-
-# L1.next = L2
-# L2.prevNode = L1
-# L2.next = L3
-# L3.prevNode = L2
-
-
-# def check_cycle(node):
-#     pointer1 = node
-#     pointer2 = node 
-
-#     while pointer2 != None and pointer2.next != None:
-#         pointer1 = node.next
-#         pointer2 = node.next.next
-
-#         if pointer1 == pointer2:
-#             return True 
-
-
-#     return False
 
 
 # def reverse(head):
@@ -123,7 +79,7 @@ class DoublyLinkedList:
     
         else:
             temp = self.head
-            temp.next.prev = None # remove previous pointer referring to old head
+            temp.next = None # remove previous pointer referring to old head
             self.head = temp.next # make second element the new head
             temp.next = None # remove next pointer referring to new head
         return temp.data
@@ -172,5 +128,19 @@ class DoublyLinkedList:
       
         if temp_node == self.head: # checks whether new node is being added before the first element
             self.head = new_node # makes new node the new head
+        
+    def check_cycle(self,node):
+       pointer1 = node
+       pointer2 = node 
+
+       while pointer2 != None and pointer2.next != None:
+          pointer1 = node.next
+          pointer2 = node.next.next
+
+          if pointer1 == pointer2:
+              return True 
+
+
+       return False
 
 
